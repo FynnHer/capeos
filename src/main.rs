d7adeab6e3b252a43c45855eef2604fdd7615b2e
FynnHer <fynnlasse.herrmann@gmail.com>
2025-11-30 22:35:18 +0100

    Refactor project structure: remove unused configuration, update Cargo.toml, and implement disk image functionality

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@
 #![no_std]
 #![no_main]
 
+#![feature(abi_efiapi)]
+
 use core::panic::PanicInfo;
-
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-
-    loop {}
-}
+use core::ffi::c_void;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "efiapi" fn efi_main(
+    image: *mut c_void,
+    system_table: *const c_void,
+) -> usize {
     loop {}
 }
